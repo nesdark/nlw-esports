494333178f5894f91b02403fafd174efbd0a40aa
Christian <nesd4rkfocus@gmail.com>
2022-11-06 23:40:20 -0300

    Second commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let cards = document.querySelectorAll("section")
+for (c = 1; c === 3; c++) {
+  let delay = 0
+  console.log("delay", delay)
+  // console.log("Actual Card", c)
+  // cards[c].setAttribute("style", `animation-delay: ${delay}s;`)
+  // delay += 0.6
+}
